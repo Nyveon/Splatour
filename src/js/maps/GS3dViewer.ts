fdab70452f807f12f60a70ad2793e8f21e2a5936
Eric K <54124297+Nyveon@users.noreply.github.com>
2024-10-17 17:37:32 -0300

    refactor: viewer to it's own element insteadof full-window

--- src/js/maps/GS3dViewer.ts
+++ src/js/maps/GS3dViewer.ts
@@ -8,48 +8,94 @@ import FirstPersonController from "../player/FirstPersonController.js";
 export default class GS3dViewer {
 	private map: GS3dMap;
 	private scene: THREE.Scene;
-    private camera: THREE.PerspectiveCamera;
-    private controls: FirstPersonController;
+	private camera: THREE.PerspectiveCamera;
+	private controls: FirstPersonController;
 	private renderer: THREE.WebGLRenderer;
+
+	private stats?: Stats;
+	private viewerContainer: HTMLElement | null;
 
 	constructor(map: GS3dMap, debug: boolean) {
 		this.map = map;
+		this.viewerContainer = document.getElementById("viewer");
+		this.initializeRenderer();
+		this.initializeScene(debug);
+		this.initializeCamera();
+		this.initializeControls();
+		this.handleResize();
+		this.startRenderLoop(debug);
+		this.map.addToScene(this.scene);
+	}
 
-        let stats;
+	private initializeRenderer() {
+		this.renderer = new THREE.WebGLRenderer({ antialias: true });
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		this.viewerContainer?.appendChild(this.renderer.domElement);
+	}
+
+	private initializeScene(debug: boolean) {
+		this.scene = new THREE.Scene();
+
 		if (debug) {
-			stats = new Stats();
-			stats.showPanel(0);
-			document.body.appendChild(stats.dom);
+			this.scene.add(new THREE.AxesHelper(500));
+			this.scene.add(createCheckerboard());
+
+			this.stats = new Stats();
+			this.stats.showPanel(0);
+			this.viewerContainer?.appendChild(this.stats.dom);
 		}
 
-		this.renderer = new THREE.WebGLRenderer();
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
-		document.body.appendChild(this.renderer.domElement);
+		this.scene.add(createSky(this.renderer));
+	}
 
-		this.scene = new THREE.Scene();
-		this.scene.add(new THREE.AxesHelper(500));
-		if (debug) this.scene.add(createSky(this.renderer));
-		this.scene.add(createCheckerboard());
+	private initializeCamera() {
+		const aspectRatio = window.innerWidth / window.innerHeight;
+		this.camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+	}
 
-        const aspectRatio = window.innerWidth / window.innerHeight;
-        this.camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+	private initializeControls() {
+		//todo: debug unlocks vertical movement
+		this.controls = new FirstPersonController(
+			this.camera,
+			this.renderer.domElement
+		);
+		this.controls.setTranslation(0, 3.5, 10);
+	}
 
-        window.addEventListener("resize", () => {
-            this.renderer.setSize(window.innerWidth, window.innerHeight);
-            this.camera.aspect = window.innerWidth / window.innerHeight;
-            this.camera.updateProjectionMatrix();
-        });
+	private handleResize() {
+		const resizeHandler = () => {
+			if (!this.viewerContainer) return;
 
-        this.controls = new FirstPersonController(this.camera, this.renderer.domElement);
-        this.controls.setTranslation(0, 3.5, 10);
+			const newWidth = this.viewerContainer.clientWidth;
+			const newHeight = this.viewerContainer.clientHeight;
+			this.renderer.setSize(newWidth, newHeight);
+			this.camera.aspect = newWidth / newHeight;
+			this.camera.updateProjectionMatrix();
+		};
 
-        this.renderer.setAnimationLoop(() => {
-            if (debug) stats.begin();
-            this.controls.update();
-            this.renderer.render(this.scene, this.camera);
-            if (debug) stats.end();
-        });
+		window.addEventListener("resize", this.throttle(resizeHandler, 200));
+	}
 
-        this.map.addToScene(this.scene);
+	// Utility function to throttle event handlers
+	private throttle(func: Function, limit: number) {
+		let inThrottle: boolean;
+		return function (this: any, ...args: any[]) {
+			if (!inThrottle) {
+				func.apply(this, args);
+				inThrottle = true;
+				setTimeout(() => (inThrottle = false), limit);
+			}
+		};
+	}
+
+	private startRenderLoop(debug: boolean) {
+		this.renderer.setAnimationLoop(() => {
+			if (debug && this.stats) this.stats.begin();
+
+			this.controls.update();
+			this.renderer.render(this.scene, this.camera);
+
+			if (debug && this.stats) this.stats.end();
+		});
 	}
 }

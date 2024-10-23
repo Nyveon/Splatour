5dc8d9024fc91418092d66dd9680a754cb802ee7
Eric K <54124297+Nyveon@users.noreply.github.com>
2024-10-23 18:25:15 -0300

    feat: toast spawning and export button functionality

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,7 +3,10 @@ import GS3dMap from './js/maps/GS3dMap'
 import GS3dViewer from './js/maps/GS3dViewer'
 
 declare global {
-  var Alpine: AlpineType
-  var GS3dMap: typeof GS3dMap
-  var GS3dViewer: typeof GS3dViewer
+    var Alpine: AlpineType
+    var GS3dMap: typeof GS3dMap
+    var GS3dViewer: typeof GS3dViewer
+    var successToast: (message: string) => void
+    var warningToast: (message: string) => void
+    var copyToClipboard: (valueName: string, value: string, type: string) => void
 }

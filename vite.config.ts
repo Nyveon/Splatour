108c0454e02cf1006dee5c319858b766a9e5789a
Eric K <54124297+Nyveon@users.noreply.github.com>
2024-12-01 21:00:30 -0300

    feat: re-work build process for example files + cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,26 @@ import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
 	const isViewer = mode === "viewer";
 
+	const staticFiles = [
+		{ src: "./public/coi-serviceworker.min.js", dest: "./" },
+	];
+
+	if (!isViewer) {
+		staticFiles.push({ src: "./public/CNAME", dest: "./" });
+
+		// mobile.json
+		staticFiles.push({ src: "./public/converted_file.ksplat", dest: "./" });
+		staticFiles.push({ src: "./public/test.ksplat", dest: "./" });
+	}
+
 	return {
+		publicDir: false,
 		plugins: [
 			react(),
 			basicSsl(),
@@ -21,17 +35,17 @@ export default defineConfig(({ mode }) => {
 				},
 			}),
 			visualizer({
-				open: true,
+				open: false,
 				filename: "bundle-visualization.html",
 				template: "network",
+			}),
+			viteStaticCopy({
+				targets: staticFiles,
 			}),
 		],
 		build: {
 			outDir: isViewer ? "docs/export" : "docs",
 			emptyOutDir: true,
-			rollupOptions: {
-				treeshake: true,
-			},
 		},
 		server: {
 			port: 8080,
